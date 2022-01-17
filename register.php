70c8f02ecce309c2e3da0cbb24596e1b823bb9f1
Luke <89837081+Frietvorkje69@users.noreply.github.com>
2022-01-17 13:10:16 +0100

    Create register.php

---
+++ register.php
@@ -0,0 +1,65 @@
+<?php
+
+if(isset($_POST['submit'])) {
+    require_once "DB/database.php";
+
+    /** @var mysqli $db */
+
+    $email = mysqli_escape_string($db, $_POST['email']);
+    $password = $_POST['password'];
+
+    $errors = [];
+    if($email == '') {
+        $errors['email'] = 'Voer een gebruikersnaam in';
+    }
+    if($password == '') {
+        $errors['password'] = 'Voer een wachtwoord in';
+    }
+
+    if(empty($errors)) {
+        $password = password_hash($password, PASSWORD_DEFAULT);
+
+        $query = "INSERT INTO users (email, password) VALUES ('$email', '$password')";
+
+        $result = mysqli_query($db, $query)
+        or die('Db Error: '.mysqli_error($db).' with query: '.$query);
+
+        if ($result) {
+            header('Location: login.php');
+            exit;
+        }
+    }
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
+    <title>Registreren</title>
+</head>
+<body style="text-align:center">
+<section>
+<h3>Nieuwe gebruiker registeren</h3>
+<form action="" method="post">
+    <div class="data-field">
+        <label for="email">Emailadres</label>
+        <input id="email" type="text" name="email" value="<?= $email ?? '' ?>"/>
+        <span class="errors"><?= $errors['email'] ?? '' ?></span>
+    </div>
+    <div class="data-field">
+        <label for="password">Wachtwoord</label>
+        <input id="password" type="password" name="password" value="<?= $password ?? '' ?>"/>
+        <span class="errors"><?= $errors['password'] ?? '' ?></span>
+    </div>
+    <div class="data-submit smallZoom">
+        <input type="submit" name="submit" value="Registreren"/>
+    </div>
+</form>
+</section>
+<section><a href="login.php">Terug naar site</a></section>
+</body>
+</html>
