e6c6f1673b84810fa74351ff6b5040faec9b49ab
Luke <89837081+Frietvorkje69@users.noreply.github.com>
2022-01-17 13:05:41 +0100

    Create about.php

---
+++ about.php
@@ -0,0 +1,45 @@
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <link
+            rel="icon"
+            href="https://cdn.glitch.me/dd3e24be-0f5e-4407-ac88-7e0d7057b1ff%2Ficon.png?v=1638789020824"
+    />
+    <title>DVV | Over ons</title>
+    <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
+    <script src="https://kit.fontawesome.com/c7b1d33b1c.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Gloria+Hallelujah&family=Mansalva&display=swap"
+          rel="stylesheet">
+</head>
+<nav>
+    <div class="bigZoom"><a href="index.html">Home</a></div>
+    <div class="bigZoom"><a href="reservation.php">Reserveren</a></div>
+    <div class="bigZoom"><a href="about.php">Over ons</a></div>
+    <div class="bigZoom"><a href="login.php">Log in</a></div>
+</nav>
+<header><h1>Over ons</h1></header>
+<body>
+<section>
+    <h1>
+        Wie zijn wij?
+    </h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+        laborum.
+        <br>
+        <fig>
+            <img
+                    src="https://cdn.glitch.com/dd3e24be-0f5e-4407-ac88-7e0d7057b1ff%2FLUKEE.jpg?v=1633339356007"
+            />
+    <figcaption>Afbeelding 1: Onze trotse CEO; Luke de Groot</figcaption>
+    </fig>
+    </p>
+</section>
+</body>
+</html>
